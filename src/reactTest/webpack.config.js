b54edd688a3261a3c1247e7a691f540211a9d68e
Robert Maissan <robert.maissan@gmail.com>
2016-09-08 20:19:10 -0400

    Initial commit

---
+++ src/reactTest/webpack.config.js
@@ -0,0 +1,25 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './ReactApp/app.jsx',
+	output: {
+		path: path.resolve(__dirname, 'wwwroot/js'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		loaders: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015', 'react']
+					}
+				}
+		]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: 'source-map'
+};
